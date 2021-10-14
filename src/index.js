a3de6db9adf8b9c9b422d8683c138fcc4c4f53e9
Daniel Won <dan.j.won@gmail.com>
2021-10-14 06:59:47 -0500

    convert functional to class component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-	return <div>Hello world!</div>;
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { lat: null }; // value of lat hasn't been defined yet
+	}
+	// render() must be defined
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position), //success callback
+			(err) => console.log(err),
+		);
+		return <div>Latitude: </div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
