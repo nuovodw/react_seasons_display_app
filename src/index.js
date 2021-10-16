08278590fc9e36ef3de9d7f5f716b0bf09c3ebaf
Daniel Won <dan.j.won@gmail.com>
2021-10-16 06:02:58 -0500

    update state properties

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ class App extends React.Component {
 		super(props);
 
 		this.state = { lat: null }; // value of lat hasn't been defined yet
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude });
+			}, //success callback
+			(err) => console.log(err),
+		);
 	}
 	// render() must be defined
 	render() {
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => console.log(position), //success callback
-			(err) => console.log(err),
-		);
-		return <div>Latitude: </div>;
+		return <div>Latitude: {this.state.lat} </div>;
 	}
 }
 
