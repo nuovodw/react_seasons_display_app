1ac9bb7f8ce0a041fb6669b12dc849bb80609a34
Daniel Won <dan.j.won@gmail.com>
2021-10-18 07:15:52 -0500

    pass lat as a prop into SeasonsDisplay

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 	// constructor(props) {
@@ -10,6 +10,7 @@ class App extends React.Component {
 	// }
 
 	state = { lat: null, errorMessage: '' };
+
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => this.setState({ lat: position.coords.latitude }),
@@ -24,7 +25,7 @@ class App extends React.Component {
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		return <div>Loading...</div>;
