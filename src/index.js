aa1429b93ada3474262ec6c0775aa13490e7228f
Daniel Won <dan.j.won@gmail.com>
2021-10-19 05:40:05 -0500

    clean up code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-
-	// 	this.state = { lat: null, errorMessage: '' };
-	// }
-
 	state = { lat: null, errorMessage: '' };
 
 	componentDidMount() {
