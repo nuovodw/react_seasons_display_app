de5055659ed7e0e2ceb7e58d43bf20d163a3fb59
Daniel Won <dan.j.won@gmail.com>
2021-10-19 05:51:59 -0500

    create Spinner.js

---
+++ src/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Spinner() {
+	return (
+		<div>
+			<div class="ui active dimmer">
+				<div class="ui text loader">Loading</div>
+			</div>
+		</div>
+	);
+}
+
+export default Spinner;
