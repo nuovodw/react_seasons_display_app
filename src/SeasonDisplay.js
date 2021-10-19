ad77dc94e45af806e93ec5f321c54d22e2dfc3ea
Daniel Won <dan.j.won@gmail.com>
2021-10-19 04:58:25 -0500

    add icons and create config objects

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+	summer: {
+		text: "It's Summer 😎",
+		iconName: 'sun', // Semantic UI
+	},
+	winter: {
+		text: "It's Winter ⛄",
+		iconName: 'snowflake', // Semantic UI
+	},
+};
+
 const getSeason = (lat, month) => {
 	if (month > 2 && month < 9) {
 		return lat > 0 ? 'summer' : 'winter';
@@ -10,14 +21,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	const displayText = season === 'winter' ? "It's Winter ❄" : "It's Summer 😎";
-	const icon = season === 'winter' ? 'snowflake' : 'sun'; // snowflake & sun from Semantic UI
+	const { text, iconName } = seasonConfig[season];
 
 	return (
 		<div>
-			<i className={`${icon} icon`} />
-			<h1>{displayText}</h1>
-			<i className={`${icon} icon`} />
+			<i className={`${iconName} icon`} />
+			<h1>{text}</h1>
+			<i className={`${iconName} icon`} />
 		</div>
 	);
 };
